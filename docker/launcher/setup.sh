6423d206c4fc8cb409a1baabbd7c2c5d6047bfa5
Alastair Poole <mail@alastairpoole.com>
2023-01-11 14:30:02 +0000

    docker: launcher simplify requirements.
    Some of these are superfluous.

--- docker/launcher/setup.sh
+++ docker/launcher/setup.sh
@@ -7,8 +7,7 @@ apt-get -y upgrade
 apt-get -y install python3 pip git tzdata
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-python3 -m pip install mysql-connector-python paho.mqtt boto3
-mkdir /opt
+python3 -m pip install mysql-connector-python
 cd /opt
 git clone https://github.com/haxworx/slurp --depth 1
 cp /config.ini /opt/slurp
